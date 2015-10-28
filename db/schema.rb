f9bee281910cf4f008076f3dff6fa24ff8ea9929
David Navarro <dmn88mia@gmail.com>
2015-10-28 01:38:43 -0400

    added price in table and created div loop

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028032117) do
+ActiveRecord::Schema.define(version: 20151028044611) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151028032117) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "colour"
+    t.integer  "price"
   end
 
 end
