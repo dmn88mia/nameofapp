dbd48748b5cea32375c5b534cb2e02f97d008285
David Navarro <dmn88mia@gmail.com>
2016-03-08 14:59:10 -0500

    added byebug for debugging

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,6 +20,7 @@ class ProductsController < ApplicationController
 
   # GET /products/new
   def new
+    # byebug
     @product = Product.new
   end
 
@@ -46,6 +47,7 @@ class ProductsController < ApplicationController
   # PATCH/PUT /products/1
   # PATCH/PUT /products/1.json
   def update
+    # byebug
     respond_to do |format|
       if @product.update(product_params)
         format.html { redirect_to @product, notice: 'Product was successfully updated.' }
